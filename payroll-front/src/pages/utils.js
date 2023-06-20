e2280469b157c5e0d8d82057cb045feedd7a505d
rohit <125443092+rohit-zd@users.noreply.github.com>
2023-06-20 12:11:25 +0530

    Update utils.js

--- payroll-front/src/pages/utils.js
+++ payroll-front/src/pages/utils.js
@@ -4,7 +4,7 @@
 // var host = "http://localhost:7074" 
  
 
-var host = "http://payroll.zecdata.com:7074"
+var host = "http://144.126.254.221:7074"
 
 //var host = `${process.env.HOST}:${process.env.PORT}`;  //Sanjay
 export default host
