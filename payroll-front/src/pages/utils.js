155a5dfa7fbbf386d798887b5f78de7bd5363930
rohit-zd <rohit.k@zecdata.com>
2023-06-20 18:05:30 +0530

    change http to https

--- payroll-front/src/pages/utils.js
+++ payroll-front/src/pages/utils.js
@@ -3,7 +3,7 @@
 //var host = "" 
 // var host = "http://localhost:7074" 
  
- var host = "http://payroll.zecdata.com:7074"
+ var host = "https://payroll.zecdata.com:7074"
 
 // //var host = `${process.env.HOST}:${process.env.PORT}`;  //Sanjay
 export default host
